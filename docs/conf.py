a9c9091f402bc3dcece140bcfb8c02d5eec7546a
Stephan Seifermann <stephan.seifermann@kit.edu>
2020-05-02 20:37:06 +0200

    debug

--- docs/conf.py
+++ docs/conf.py
@@ -118,6 +118,7 @@ html_sidebars = {
 
 def get_github_info():
 	git_repo = git.Repo(__file__, search_parent_directories=True)
+	print(git_repo.remotes)
 	remote_url = git_repo.remotes.origin.url
 	regex = r"([^\/:]+)\/([^\/]+)\.git"
 	result = re.findall(regex, remote_url)
